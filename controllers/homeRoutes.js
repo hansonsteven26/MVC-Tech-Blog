0c93520130a0826366c578f8e1ce20db493608f9
steven <smhanson21@gmail.com>
2023-11-27 17:53:17 -0600

    add get route to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,13 +2,25 @@ const router = require('express').Router();
 const authenticated = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    try {
-        res.render('homepage', {
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
