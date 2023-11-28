4afa4b367bf8ea4bf4df181a86d35d6fe2b53eef
steven <smhanson21@gmail.com>
2023-11-28 04:21:16 -0600

    chatgpt connection to try and get heroku to work

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,28 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // If DATABASE_URL is set (Heroku), use it
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
-    port: process.env.PORT,
-  }
-);
+    dialectOptions: {
+      socketPath: '/cloudsql/your-project-id:your-region:your-instance-name', // If using Google Cloud SQL
+    },
+  });
+} else {
+  // Use local database configuration
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: process.env.DB_PORT || 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
