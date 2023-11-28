2a2f547122303cf532affc9c9b8d50554a48d9ec
steven <smhanson21@gmail.com>
2023-11-28 14:35:04 -0600

    put mongoose.connect in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech_db');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
