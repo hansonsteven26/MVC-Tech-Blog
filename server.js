ec42f9252792989c8c653a15ecbb704cd4de9a97
steven <smhanson21@gmail.com>
2023-11-28 17:13:33 -0600

    include JAWSDB_URL

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const mongoose = require('mongoose');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech_db');
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
