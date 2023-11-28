99b0a3113991a733f7adc69e05aeaf0c109918f0
steven <smhanson21@gmail.com>
2023-11-28 01:20:24 -0600

    finish commentRoutes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,11 +1,71 @@
-const router = require("express").Router();
-const { Comment } = require("../../models");
-const withAuth = require("../../utils/auth");
+const router = require('express').Router();
+const { Comment, User } = require('../../models');
+const authenticated = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const commentData = await Comment.findAll({})
+    const commentData = await Comment.findAll({
+      include: {
+        model: User,
+        attributes: ['username'],
+      },
+    });
+
+    res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        model: User,
+        attributes: ['username'],
+      },
+    });
+
+    if (!commentData) {
+      return res.status(404).json({ message: 'No comment found with that ID' });
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', authenticated, async (req, res) => {
+  try {
+    const commentData = await Comment.create({
+      ...req.body,
+    });
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', authenticated, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!commentData) {
+      return res.status(404).json({ message: 'No comment found with that ID' });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
